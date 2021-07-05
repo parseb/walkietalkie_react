8e4bb85a9a480976064907613fc8a48146d85896
Bogdan Arsene <arenebp@gmail.com>
2021-07-05 14:24:15 +0300

    walkietalike react components init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,8 @@
 class ApplicationController < ActionController::Base
 
     color=['white','blue','red','yellow','black','purple','green'].sample
-    number= rand(1000).sample
+    number= rand(1000)
     animal=['rabbit','pig','rooster','lion','cougar'].sample
-    session[:current_user] = color + '-' + number.to_s + '-' + animal 
-
-    
+    current_user = color + '-' + number.to_s + '-' + animal 
 
 end
